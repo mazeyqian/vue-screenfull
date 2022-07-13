95e54ad3826ffa163d888ebba5c6d4a4452e5d4e
Mazey/后除 <mazey@mazey.net>
2022-07-13 23:34:39 +0800

    style: add lib link

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 /* Vue Screenfull */
-
+// JS Lib https://github.com/rafgraph/fscreen
+// JS Lib Demo https://fscreen.rafgraph.dev/
+// React Component by fscreen↑ https://github.com/snakesilk/react-fullscreen
+// JS Lib https://github.com/sindresorhus/screenfull
+// Vue3 Component by screenfull.js↑ https://github.com/mirari/vue-fullscreen/tree/v3
 import { isNumber } from 'mazey';
 import { defineComponent } from 'vue'; // TypeScript with Options API https://vuejs.org/guide/typescript/options-api.html
 
