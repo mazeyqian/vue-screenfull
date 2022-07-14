e2a0fc615ee0c74ff78d2dfbec76b0bb9f31d7bf
Mazey/后除 <mazey@mazey.net>
2022-07-14 23:49:05 +0800

    feat: add `template`

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,28 @@
 import { isNumber } from 'mazey';
 import { defineComponent } from 'vue'; // TypeScript with Options API https://vuejs.org/guide/typescript/options-api.html
 
+// https://vuejs.org/guide/typescript/overview.html#definecomponent
 export default defineComponent({
   // type inference enabled
+  template: `
+    <div>
+      <span>{{ FirstName }}</span>
+    </div>
+  `,
   props: {
     name: String,
     msg: { type: String, required: true }
   },
   data() {
     return {
-      count: 1
+      count: 1,
+      FirstName: 'm',
     };
   },
   mounted() {
     this.name; // type: string | undefined
     this.msg; // type: string
     this.count; // type: number
-  }
+    this.FirstName = 'Mazey';
+  },
 });
